eb6489ecd27abebd5f1850ab63f75744ee45163c
Dmitry Kulinenko <67313082+Dmitry-Kulinenko@users.noreply.github.com>
2022-10-04 21:35:30 +0300

    Delete jonline-strings/src/by/jonline/string/stringAsCharArray directory

--- jonline-strings/src/by/jonline/string/stringAsCharArray/Test2.java
+++
@@ -1,8 +0,0 @@
-package by.jonline.string.stringaschararray;
-
-public class Test2 {
-	public static void main(String[] args) {
-		int[] a = { 1, 2, 3, 4, 5, 6 };
-		System.out.println(a[0]);
-	}
-}
